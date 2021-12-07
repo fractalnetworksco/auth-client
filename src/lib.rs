4786b00343b11af4106a148a14cc63a641f80ad5
Patrick Elsen <pelsen@xfbs.net>
2021-12-07 14:53:38 +0100

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,72 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::serde::uuid::Uuid;
+use thiserror::Error;
+use std::collections::HashSet;
+
+/// Scopes of access
+#[derive(Clone, Debug, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub enum Scope {
+    Link,
+    LinkRead,
+    Account,
+    AccountRead,
+    Network,
+    NetworkRead,
+}
+
+/// Generic request context. May contain idempotency token, request ID, a JWT
+/// pulled from the cookies, or a token pulled from the header.
+pub struct Auth {
+    uuid: Uuid,
+    scopes: HashSet<Scope>,
+}
+
+impl Auth {
+    pub fn user(&self) -> Uuid {
+        self.uuid.clone()
+    }
+
+    pub fn scopes(&self) -> &HashSet<Scope> {
+        &self.scopes
+    }
+
+    pub fn allowed(&self, scope: Scope) -> Result<(), AuthError> {
+        Ok(())
     }
 }
+
+/// Errors that can happen when validating authentication
+#[derive(Error, Clone, Debug)]
+pub enum AuthError {
+    #[error("JWT is wrong")]
+    TokenInvalid,
+    #[error("JWT is missing")]
+    TokenMissing,
+    #[error("JWT is expired")]
+    TokenExpired,
+    #[error("JWT missing scope")]
+    Unauthorized,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Auth {
+    type Error = AuthError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let token = match req.headers().get_one("Authorization").map(|h| h.to_string()) {
+            Some(s) => s,
+            None => return Outcome::Failure((Status::Unauthorized, AuthError::TokenMissing)),
+        };
+        let uuid: Uuid = match token.parse() {
+            Ok(uuid) => uuid,
+            Err(_) => return Outcome::Failure((Status::Unauthorized, AuthError::TokenInvalid)),
+        };
+        Outcome::Success(Auth {
+            uuid,
+            scopes: HashSet::from([Scope::Link]),
+        })
+    }
+}
+
+
