694db2cc349332778dca7075d0f25f7387915d1b
grantkee <grant@ether.ai>
2022-01-05 10:48:32 -0600

    add SystemContext

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,8 @@ pub enum AuthError {
     RequestError(#[from] reqwest::Error),
     #[error("Auth token invalid")]
     InvalidAuthToken,
+    #[error("Scope is wrong")]
+    ScopeError,
 }
 
 /// Scopes of access
@@ -205,6 +207,122 @@ impl<'r> FromRequest<'r> for UserContext {
             None => return Outcome::Failure((Status::Unauthorized, AuthError::TokenMissing)),
         };
         let mut auth = match UserContext::from_token(config, &token, ip).await {
+            Ok(auth) => auth,
+            Err(e) => return Outcome::Failure((Status::Unauthorized, e)),
+        };
+
+        // get idempotency token
+        match req.headers().get_one("Idempotency-Token") {
+            Some(token) => {
+                auth.parse_idempotency_token(&token.to_string());
+            }
+            None => {}
+        }
+
+        Outcome::Success(auth)
+    }
+}
+
+/// System request context. May contain idempotency token, request ID, a JWT
+/// pulled from the cookies, or a token pulled from the header. This context
+/// requires 'system' claim.
+#[cfg_attr(feature = "openapi", derive(OpenApiFromRequest))]
+pub struct SystemContext {
+    account: Uuid,
+    scope: Option<Scope>,
+    idempotency_token: Option<Uuid>,
+    ip_addr: IpAddr,
+}
+
+impl SystemContext {
+    /// Get request user's account UUID
+    pub fn account(&self) -> Uuid {
+        self.account.clone()
+    }
+
+    pub fn scope(&self) -> &Option<Scope> {
+        &self.scope
+    }
+
+    /// Get request idempotency token
+    pub fn idempotency(&self) -> Option<Uuid> {
+        self.idempotency_token.clone()
+    }
+
+    /// Get request IP
+    pub fn ip(&self) -> IpAddr {
+        self.ip_addr
+    }
+
+    pub fn allowed(&self, _scope: Scope) -> Result<(), AuthError> {
+        Ok(())
+    }
+
+    /// Try to validate token
+    pub async fn from_token(
+        config: &AuthConfig,
+        token: &str,
+        ip_addr: IpAddr,
+    ) -> Result<SystemContext, AuthError> {
+        let jwt = config.keystore.verify(token)?;
+        let account = jwt.payload().sub().ok_or(AuthError::PayloadError)?;
+        let account = Uuid::parse_str(account).map_err(|_| AuthError::PayloadError)?;
+        let scopes = jwt
+            .payload()
+            .get_str("scope")
+            .ok_or(AuthError::PayloadError)?;
+        let scope_vec: Vec<&str> = scopes.split_whitespace().collect();
+
+        if !scope_vec.contains(&"system") {
+            return Err(AuthError::ScopeError);
+        }
+
+        Ok(SystemContext {
+            account,
+            scope: None,
+            idempotency_token: None,
+            ip_addr,
+        })
+    }
+
+    pub fn parse_idempotency_token(&mut self, token: &str) -> Result<(), AuthError> {
+        let token = Uuid::from_str(token)?;
+        self.idempotency_token = Some(token);
+        Ok(())
+    }
+}
+
+#[cfg(feature = "rocket")]
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for SystemContext {
+    type Error = AuthError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let ip = match IpAddr::from_request(req).await {
+            Outcome::Success(ip) => ip,
+            _ => unimplemented!(),
+        };
+
+        let config = match req.rocket().state::<AuthConfig>() {
+            Some(config) => config,
+            None => {
+                return Outcome::Failure((Status::InternalServerError, AuthError::MissingConfig))
+            }
+        };
+
+        let token = match req
+            .headers()
+            .get_one("Authorization")
+            .map(|h| h.to_string())
+        {
+            Some(s) => s,
+            None => return Outcome::Failure((Status::Unauthorized, AuthError::TokenMissing)),
+        };
+        let token = match token.strip_prefix("Bearer ") {
+            Some(token) => token,
+            None => return Outcome::Failure((Status::Unauthorized, AuthError::TokenMissing)),
+        };
+        let mut auth = match SystemContext::from_token(config, &token, ip).await {
             Ok(auth) => auth,
             Err(e) => return Outcome::Failure((Status::Unauthorized, e)),
         };
